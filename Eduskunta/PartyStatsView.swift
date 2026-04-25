395713cfc4f632cd1c4a7e1d02fb34fef95cb1f1
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-04-25 15:07:06 +0300

    Update commenting

---
+++ Eduskunta/PartyStatsView.swift
@@ -0,0 +1,18 @@
+//
+//  PartyStatsView.swift
+//  Eduskunta
+//
+//  Created by Victoria Vavulina on 25.4.2026.
+//
+
+import SwiftUI
+
+struct PartyStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PartyStatsView()
+}
